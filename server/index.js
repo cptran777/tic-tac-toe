0f03c0a78da9de308c74514c1a745b96f5253c27
cptran777 <cptran14@gmail.com>
2016-12-02 12:57:51 -0800

    init basic application skeleton and webpack

---
+++ server/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/******************* INIT DEPENDENCIES *********************/
+
+// Main server dependencies
+let express = require('express');
+let path = require('path');
+let bodyparser = require('body-parser');
+require('dotenv').config();
+
+/******************* INIT MIDDLEWARE ***********************/
+
+let app = express();
+app.use(express.static(__dirname + '/../client/public'));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyparser.json());
+
+/********************* INIT ROUTES *************************/
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+});
+
+/********************* INIT SERVER *************************/
+
+let port = process.env.PORT || 3000;
+let server = app.listen(port, () => {
+  console.log('Listening on port', port);
+});
